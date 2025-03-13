f24955fddf3b70a834f3361c90f7591ef92bea09
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-13 11:09:37 +0900

    svgrをセットアップ

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev"
+import type { Config as SvgrConfig } from "@svgr/core"
 import type { NextConfig } from "next"
+import type { TurboLoaderItem } from "next/dist/server/config-shared"
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
@@ -9,8 +11,33 @@ if (process.env.NODE_ENV === "development") {
     setupDevPlatform()
 }
 
+const svgrConfig: SvgrConfig & Extract<TurboLoaderItem, { options: unknown }>["options"] = {
+    svgo: true,
+    svgoConfig: {
+        plugins: [
+            {
+                name: "removeUnknownsAndDefaults",
+                params: { keepAriaAttrs: true, keepRoleAttr: true },
+            },
+        ],
+    },
+}
 const nextConfig: NextConfig = {
-    /* config options here */
+    experimental: {
+        turbo: {
+            rules: {
+                "*.svg": {
+                    loaders: [
+                        {
+                            loader: "@svgr/webpack",
+                            options: svgrConfig,
+                        },
+                    ],
+                    as: "*.js",
+                },
+            },
+        },
+    },
 }
 
 export default nextConfig
