55cc3f37c02ab00f124b65a31755987c7ecf1a18
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-13 18:14:04 +0900

    Geist Monoフォントを使用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 
 import { Footer } from "@/components/Footer"
 
-const geistSans = Geist({ subsets: ["latin"] })
-
-const geistMono = Geist_Mono({ subsets: ["latin"] })
+const geistMono = Geist_Mono({
+    weight: "variable",
+    display: "swap",
+    subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
     title: "第79回灘校文化祭 weave",
@@ -18,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.className} ${geistMono.className} antialiased`}>
+            <body className={`${geistMono.className} antialiased`}>
                 {children}
                 <Footer />
             </body>
