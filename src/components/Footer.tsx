735836c43831fde39b206cb43b8e0ba91f362f03
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-14 18:05:23 +0900

    ライトモードでのフッターの文字色を修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import WeaveLogo from "./svg/weave-logo.svg"
 import WeaveLogotype from "./svg/weave-logotype.svg"
 
 export const Footer = () => (
-    <footer className="flex flex-col justify-center gap-8 bg-zinc-950 p-8 md:flex-row md:justify-between md:px-16">
+    <footer className="flex flex-col justify-center gap-8 bg-zinc-950 p-8 text-neutral-100 md:flex-row md:justify-between md:px-16">
         <div className="flex justify-center gap-2">
             <WeaveLogo className="my-auto size-full max-h-24 max-w-24" />
             <div className="flex w-min flex-col justify-center gap-3">
