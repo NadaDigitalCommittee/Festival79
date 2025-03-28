41a24798549ee8674bf0a96402acdae6c5e0fcf1
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-29 08:06:43 +0900

    「1日目」「2日目」の右端を揃える

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,11 +49,15 @@ export default function Home() {
                 </span>
                 <div className="grid grid-cols-[max-content_1fr] grid-rows-[1fr_1fr_max-content] gap-8">
                     <div className="col-span-2 grid grid-cols-subgrid items-center gap-8">
-                        <span className="text-2xl font-bold dark:text-dark-secondary">1日目</span>
+                        <span className="text-right text-2xl font-bold dark:text-dark-secondary">
+                            1日目
+                        </span>
                         <span className="text-4xl dark:text-dark-secondary">9:00 ~ XX:XX</span>
                     </div>
                     <div className="col-span-2 grid grid-cols-subgrid items-center gap-8">
-                        <span className="text-2xl font-bold dark:text-dark-secondary">2日目</span>
+                        <span className="text-right text-2xl font-bold dark:text-dark-secondary">
+                            2日目
+                        </span>
                         <span className="text-4xl dark:text-dark-secondary">9:00 ~ XX:XX</span>
                     </div>
                     <span className="col-span-2 dark:text-dark-secondary">
