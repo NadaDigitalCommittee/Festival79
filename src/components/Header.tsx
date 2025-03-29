a0abf6a6deea8310249b6031a12a6b7e3158df8c
Tito5e <tosirusan@gmail.com>
2025-03-30 08:38:53 +0900

    開催カウンターとヘッダーを実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link"
+
+import { EventDateCounter } from "@/components/EventDateCounter"
+import { WeaveLogo } from "@/components/WeaveLogo"
+import { WeaveLogotype } from "@/components/WeaveLogotype"
+
 export const Header = () => {
-    return <header className="min-h-20"></header>
+    return (
+        <header className="container flex min-h-20 items-center justify-between">
+            <div className="flex items-center gap-4">
+                <Link href="/" className="flex items-center gap-2">
+                    <WeaveLogo className="size-9" />
+                    <WeaveLogotype className="h-4" />
+                </Link>
+                <EventDateCounter />
+            </div>
+            {/*
+            現在その他ページの実装が行われていないため無効化
+            その他ページの実装時にハンバーガーのコードを追加します
+            (PCビューでは展開して表示します)
+            <div className="flex gap-4">
+                <Link href="/circle">サークル</Link>
+                <Link href="/event">イベント</Link>
+                <Link href="/timetable">タイムテーブル</Link>
+                <Link href="/map">マップ</Link>
+                <Link href="/goods">グッズ</Link>
+            </div> */}
+        </header>
+    )
 }
