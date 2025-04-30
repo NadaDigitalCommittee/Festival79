841602af1b3feb45d4b8e230f31243fe51aae4ec
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-04-30 16:08:54 +0900

    ファイルサイズを表示

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises"
+import path from "node:path"
+
 import { WeaveLogo } from "@/components/WeaveLogo"
 import { WeaveLogotype } from "@/components/WeaveLogotype"
+
+const formatBytes = (bytes: number, fractionDigits: number = 2) => {
+    const k = 1000
+    const prefixes = ["", "k", "M", "G", "T", "P", "E", "Z", "Y", "R", "Q"]
+    const i = Math.max(0, Math.min(prefixes.length - 1, Math.floor(Math.log(bytes) / Math.log(k))))
+    return `${+(bytes / k ** i).toFixed(fractionDigits)} ${prefixes[i] ?? ""}B`
+}
+const calcFileSize = async (filePath: string) => {
+    const stats = await fs.stat(path.join(process.cwd(), "public", filePath))
+    return formatBytes(stats.size)
+}
+
+const pamphletPath = "/gallery/pamphlet.pdf"
+const pamphletFileSize = await calcFileSize(pamphletPath)
 
 export default function Download() {
     return (
@@ -20,11 +37,11 @@ export default function Download() {
                         パンフレット
                     </span>
                     <a
-                        href="/gallery/pamphlet.pdf"
+                        href={pamphletPath}
                         download="第79回灘校文化祭『weave』 パンフレット.pdf"
                         className="self-start rounded-md bg-theme px-4 py-2 text-white"
                     >
-                        Download
+                        Download ({pamphletFileSize})
                     </a>
                 </div>
             </div>
