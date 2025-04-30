b7f66bbab5a64d4836fc4bc3d17d497ea0e662f7
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-04-30 19:29:56 +0900

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,10 @@ export default function NotFound() {
     return (
         <>
             <title>404</title>
-            <main>404</main>
+            <main className="container flex flex-col items-center justify-center gap-8">
+                <h1 className="text-9xl font-bold">404</h1>
+                <p className="text-xl">ページが見つかりません。</p>
+            </main>
         </>
     )
 }
